99e75daa8a8c4a8e2b41332238e0ff04f8c2283b
chandan <chandankumar936672@gmail.com>
2025-07-07 23:09:14 +0530

    Practice question from chatgpt

---
+++ practice.java
@@ -0,0 +1,34 @@
+package OOPS;
+
+public class chat {
+    public static void main(String args[]){
+        Puppy p1=new Puppy("brown","Laborder");
+        p1.display();
+        p1.sound();
+    }
+    
+}
+class Animal{
+    String color;
+    void sound(){
+        System.out.println( "Animal make sound");
+    }
+
+}
+class Dog extends Animal{
+    String breed;
+    void sound(){
+        System.out.println("Dog bark");
+    }
+}
+class Puppy extends Dog{
+
+    Puppy(String color,String breed){
+        this.color=color;
+        this .breed =breed;
+    }
+    void display(){
+        System.out.println("color " + this.color);
+        System.out.println("breed " + this.breed);
+    }
+}
