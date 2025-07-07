8f8eaaa5151521da531878231e2c33146d9b6011
chandan <chandankumar936672@gmail.com>
2025-07-07 23:13:16 +0530

    Polymorphism

---
+++ oops15.java
@@ -0,0 +1,9 @@
+package OOPS;
+
+public class oops15 {
+    // polymorphism --> means many forms
+    // we have two type of polymorphism
+    // 1 compile time polymorphism -----> method overloading
+    // 2 run time polymorphism  ----> method overriding
+    
+}
