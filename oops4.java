2554ad0e402c6900e17f98c8a11d9edbab8974b0
chandan <chandankumar936672@gmail.com>
2025-07-07 12:54:37 +0530

    Access Modifier

---
+++ oops4.java
@@ -0,0 +1,12 @@
+ package OOPS;
+
+public class oops4 {
+    // Acces modifier in java
+    //            with in class | with in package| out side package by subclass | outside package
+    //                          |                |                              |                |
+    // ____ ____________________|________________|______________________________|________________|
+    //  private           Y     |      N         |            N                 |          N     |
+    //  public            Y     |      Y         |            Y                 |          Y     |
+    //  default           Y     |      Y         |            N                 |          N     |
+    //  protected         Y     |      Y         |            Y                 |          N     |
+}
