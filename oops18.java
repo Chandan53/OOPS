47cf41d6759ea800bb29e8d92bb6bcbb039acab1
chandan <chandankumar936672@gmail.com>
2025-07-08 11:59:52 +0530

    Properties of abstract class

--- oops18.java
+++ oops18.java
@@ -4,7 +4,7 @@ public class oops18 {
     public static void main(String args[]){
         Animal a1=new Animal(); // error
         // property one of abstract --> cannot create an object of abstract class
-    }
+    } 
     
 }
 abstract class Animal{
