4b01f3514ec7ea0307bfba86e5ab7c31696ef5d3
chandan <chandankumar936672@gmail.com>
2025-07-08 12:04:11 +0530

    Abstract properties 2

---
+++ oops19.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class oops19 {
+    public static void main(String args[]){
+
+    }
+    
+}
+abstract class Animal{
+
+    // properties two of abstract ---> can have abstract / non abstract method
+
+    // non abstract method
+    void eat(){
+        System.out.println( " animal can eat");
+    }
+    // abstract method
+    abstract void swim();
+}
