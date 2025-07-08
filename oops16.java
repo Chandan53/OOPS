77196a11d17a975e1acbd2277b7e227bedf4068d
chandan <chandankumar936672@gmail.com>
2025-07-08 10:40:25 +0530

    method Overloading

---
+++ oops16.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+public class oops16 {
+    public static void main(String args[]){
+        Function_overloading f1=new Function_overloading();
+        System.out.println(f1.sum(2,3));
+        System.out.println(f1.sum((float)2.3 , (float)4.5));
+        System.out.println(f1.sum("chan","dan"));
+        System.out.println(f1.sum(3,4,5));
+
+    }
+    
+    
+}
+class Function_overloading{
+    int sum(int a,int b){
+        return a+b;
+    }
+
+    float sum(float a,float b){
+        return a+b;
+    }
+
+    String sum(String a,String b){
+        return a+b;
+    }
+
+    int sum(int a,int b,int c){
+        return a+b+c;
+    }
+}
